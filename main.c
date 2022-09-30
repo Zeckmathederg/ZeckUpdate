b80fd96925e8a594987ac80ae9a87d68c7c5b5e2
Zeckmathederg <zeckmaisreal@gmail.com>
2022-09-30 16:17:03 +0000

    Advanced on main.c, added fopen()

--- main.c
+++ main.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
 #include "prompts.h"
 // main.c, main function
 int main() {
-	FILE *file;
-	file = fopenf("pathtofile", "w");
-	
+	struct passwd *pw = getpwuid(getuid());
+	const char *homedir = pw->pw_dir;
+	FILE *filepath;
+	remove(*homedir + "/.zeckupdate/update.txt");
+	filepath = fopen(*homedir + "/.zeckupdate/update.txt", "w");
+	if (filepath == NULL) {
+		//printf("File not found or cannot be written to\n");
+		return 1;
+	}
+	/*
+	else {
+		printf("Success!\n");
+		return 0;
+	}
+	*/
+	printf(homedir, "%s");
 	return 0;
 }
