4505ca83d36bb46de21ada0cf919e145cef2a5d6
Zeckmathederg <zeckmaisreal@gmail.com>
2022-10-01 14:54:06 +0000

    Further work on main.c, issue came back. Will test on online compiler.

--- main.c
+++ main.c
@@ -1,26 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <pwd.h>
 #include "prompts.h"
 // main.c, main function
 int main() {
-	struct passwd *pw = getpwuid(getuid());
-	const char *homedir = pw->pw_dir;
+	const char *homedir = getenv("HOME");
 	FILE *filepath;
-	remove(*homedir + "/.zeckupdate/update.txt");
-	filepath = fopen(*homedir + "/.zeckupdate/update.txt", "w");
-	printf("%s\n", homedir);
+	filepath = fopen((int) *homedir + "/.zeckupdate/update.txt", "w");
 	if (filepath == NULL) {
 		perror("");
 		return 1;
 	}
-	/*
-	else {
-		printf("Success!\n");
-		return 0;
-	}
-	*/
 	return 0;
 }
