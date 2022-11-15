79ba2122d01538ce1ba8f2004ee818cd8d2168a3
Zeckmathederg <zeckmaisreal@gmail.com>
2022-11-14 18:04:14 -0700

    Changed prompt strings ending '\n> '

--- prompts.c
+++ prompts.c
@@ -1,17 +1,17 @@
 #include "prompts.h"
 char prompts[13][50] = {
-    "How am I feeling? ",
-    "Why do I feel this way? ",
-    "How was my day? ",
-    "Am I hungry? ",
-    "What food did I eat? ",
-    "Am I thirsty? ",
-    "What did I drink? ",
-    "Does anything hurt? ",
-    "Am I sick? ",
-    "What am I doing? ",
-    "What do I wish? ",
-    "What do I want to do? ",
-    "Am I safe? "
+    "How am I feeling?\n> ",
+    "Why do I feel this way?\n> ",
+    "How was my day?\n> ",
+    "Am I hungry?\n> ",
+    "What food did I eat?\n> ",
+    "Am I thirsty?\n> ",
+    "What did I drink?\n> ",
+    "Does anything hurt?\n> ",
+    "Am I sick?\n> ",
+    "What am I doing?\n> ",
+    "What do I wish?\n> ",
+    "What do I want to do?\n> ",
+    "Am I safe?\n> "
 };
 char inparr[13][200];
